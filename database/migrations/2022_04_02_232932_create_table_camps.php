230c49770be81166c415012520e0059242f9f738
Ion Zero <karyonosukses@gmail.com>
2022-04-03 06:55:03 +0700

    finish migration camp benefit dan checkout

--- database/migrations/2022_04_02_232932_create_table_camps.php
+++ database/migrations/2022_04_02_232932_create_table_camps.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCampBenefitsTable extends Migration
+class CreateTableCamps extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateCampBenefitsTable extends Migration
      */
     public function up()
     {
-        Schema::create('camp_benefits', function (Blueprint $table) {
+        Schema::create('camps', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('camp_id')->unsigned();
-            $table->string('name');
+            $table->string('title', 100);
+            $table->string('slug', 100);
+            $table->integer('price')->unsigned();
             $table->timestamps();
-
-            $table->foreign('camp_id')->references('id')->on('camps')->onDelete('cascade');
+            $table->softDeletes();
         });
     }
 
@@ -30,6 +30,6 @@ class CreateCampBenefitsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('camp_benefits');
+        Schema::dropIfExists('camps');
     }
 }
